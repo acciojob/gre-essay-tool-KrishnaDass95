c30810f1c511562126b63e227ee4464e0b15e1fc
Krishna Dass <43197751+KrishnaDass95@users.noreply.github.com>
2023-02-28 11:08:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-//your code here
+// lets get both the text field and the output
+let inputField = document.getElementById('evaluatedText').value;
+const outputH3 = document.getElementById('wordCount');
+
+
+function countWords(e){
+	alert(e.target.value);
+	// string s = inputField.split(' ');
+	// return s.length;
+}
+
+inputField.addEventListener('input', countWords)
